62accf77724d12c4b769a68719f8fa21d8368caa
zeet-host <setias123321@gmail.com>
2025-06-10 17:22:53 +0700

    Update token list

--- bokep.js
+++ bokep.js
@@ -1,7 +1,6 @@
 {
   "tokens": [
     "8193090449:AAEWbr9GQtfQxYNiOYQGvl9jSG-HraRX430",
-    "7582827329:AAEzwcH-pjNqi9Ti4PpDtOGkR20oPTA-rBI",
-    "1"
+    "7582827329:AAEzwcH-pjNqi9Ti4PpDtOGkR20oPTA-rBI"
   ]
 }
